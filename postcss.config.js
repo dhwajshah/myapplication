2965ef741f728a59b6e838fcf299a3202c378197
dhwaj <dhwaj8412@gmail.com>
2024-12-06 15:42:25 +0530

    Hello world Simple react + typescript app

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: {
+      tailwindcss: {},      // Tailwind CSS plugin
+      autoprefixer: {},     // Autoprefixer to add vendor prefixes to CSS rules
+    },
+  };
+  
