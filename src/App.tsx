1a2001a32ce6f30f592c02f1a118dd54f72f5309
dhwaj <dhwaj8412@gmail.com>
2024-12-09 11:23:10 +0530

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-black-600">
-      Hello World !!
-    </h1>
-  );  
-}
+    <div className="flex items-center justify-center h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold">Hello World</h1>
+    </div>
+  );
+};
 
 export default App;
